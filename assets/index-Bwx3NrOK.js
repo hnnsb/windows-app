1c869dfa5517c8c14ba88acd6343640c847e1abf
hnnsb <hannes.b@wtnet.de>
2025-07-06 01:16:01 +0200

    Updates

---
+++ assets/index-Bwx3NrOK.js
@@ -0,0 +1,1 @@
+import{j as o}from"./index-K3wo6QLW.js";const p=function(){return o.jsx("div",{})};export{p as component};
---
+++ assets/routeA-wYWisz6n.js
@@ -0,0 +1,1 @@
+import{j as t,P as o}from"./index-K3wo6QLW.js";const i=function(){return t.jsx(o,{title:"Route A",children:t.jsx("p",{children:" Some content"})})};export{i as component};
